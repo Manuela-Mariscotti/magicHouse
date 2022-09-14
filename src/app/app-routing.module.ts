bc3cd8655e7dea009bdfe08b090dfe3ce5a42e94
Daniel <danielvazquezheredia1994@gmail.com>
2022-09-14 10:48:12 +0200

    routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ResumenGastosComponent } from './pages/resumen-gastos/resumen-gastos.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: "resumen-gastos", component: ResumenGastosComponent},
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
