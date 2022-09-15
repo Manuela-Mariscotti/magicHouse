3d3711eac047606d47bc498d5d308583400d29b8
Manuela-Mariscotti <manuela.mariscotti@gmail.com>
2022-09-15 18:12:22 +0200

    page perfil

--- src/app/pages/perfil/perfil.component.ts
+++ src/app/pages/perfil/perfil.component.ts
@@ -7,7 +7,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PerfilComponent implements OnInit {
 
+  user = [
+    {name : "Pepe Soria",
+    email : "pepesoria@gmail.com",
+    password : "123",
+    phone : "688369849",
+    birth : "17/03/1987"}
+  ]
+
   constructor() { }
+
+
 
   ngOnInit(): void {
   }
