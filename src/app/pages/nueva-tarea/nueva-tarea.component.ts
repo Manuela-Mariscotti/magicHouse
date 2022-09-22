be31f6ae7902b0f7a5a69ac487830f2fd5867cd0
Manuela-Mariscotti <manuela.mariscotti@gmail.com>
2022-09-22 20:21:02 +0200

    funcionalidad new-task

--- src/app/pages/nueva-tarea/nueva-tarea.component.ts
+++ src/app/pages/nueva-tarea/nueva-tarea.component.ts
@@ -1,4 +1,12 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, Validators, FormBuilder, AbstractControl } from '@angular/forms';
+import { ApiResponse } from 'src/app/models/api-response';
+import { TasksService } from 'src/app/shared/tasks.service';
+import { Router } from '@angular/router';
+import { UserServiceService } from 'src/app/shared/user-service.service';
+import { ThisReceiver } from '@angular/compiler';
+
+
 
 @Component({
   selector: 'app-nueva-tarea',
@@ -6,11 +14,62 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./nueva-tarea.component.css'],
 })
 export class NuevaTareaComponent implements OnInit {
-  users: string[] = ['User1', 'User2', 'User3'];
-  tareas: string[] = ['task1', 'task2'];
-  dias: string[] = ['L', 'M', 'X', 'J', 'V', 'S', 'D'];
+  users = [] //= ['User1', 'User2', 'User3'];
+  tasks = [] //= ['barrer suelo', 'fregar suelo','fregar platos','poner lavadora','pasear al perro','caja del gato','limpiar cristales','cocinar','planchar','limpiar baño','recoger'];
+  days: string[] = ['L', 'M', 'X', 'J', 'V', 'S', 'D'];
 
-  constructor() {}
+  public newTaskForm : FormGroup
+  public cartelHidden : boolean = true;
+  
+  constructor(
+    private userService : UserServiceService,
+    private router : Router,
+    private formBuilder: FormBuilder,
+    private TaskService: TasksService
+  ) {
+    this.buildForm();
+    }
+  
+  usersByHome(){
+    this.userService.getUserNames()
+  }
 
-  ngOnInit(): void {}
+  newTask(){
+    const form = this.newTaskForm.value;
+    console.log(form);
+
+    this.TaskService.newTask(form.id_user,form.id_task,form.day).subscribe((res: ApiResponse)=>{
+      if (res.error) {
+        console.log(res)
+      }else {
+        console.log("Nuevo tarea asignada");
+        console.log(res);
+        this.cartelHidden = false
+
+      }
+    })
+  }
+
+  private buildForm() {
+    this.newTaskForm = this.formBuilder.group({
+      id_user : [,Validators.required],
+      id_task : [,Validators.required],
+      day : [,Validators.required]
+    })
+  }
+
+  goTasks(){
+    this.router.navigateByUrl('/tareas') 
+  }
+
+  ngOnInit(): void {
+    this.userService.getUserNames().subscribe((res : ApiResponse) =>{
+      this.users = res.data;
+      console.log(this.users);
+    })
+    this.TaskService.getTasks().subscribe((res : ApiResponse)=>{
+      this.tasks = res.data;
+      console.log(this.tasks);
+    })
+  }
 }
